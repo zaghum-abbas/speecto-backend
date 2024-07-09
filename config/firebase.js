9cb7a89f97a5540f932e8d1a1c27da9e76872ec8
zaghum-abbas <zaghumabbas524@gmail.com>
2024-07-09 17:58:04 +0500

    firbase keys into env

--- config/firebase.js
+++ config/firebase.js
@@ -1,8 +1,9 @@
 const admin = require("firebase-admin");
-// const serviceAccount = require("./serviceAccountKey.json");
+const firebaseConfig = require("./serviceAccountKey");
+
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(firebaseConfig),
 });
 
 module.exports = { admin };
